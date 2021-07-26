f3a96f43b86249c148079255db0f2ed2aefdebca
bhonestly <ernestbizzell@gmail.com>
2021-07-26 15:24:39 -0700

    import boatcontainer

---
+++ client/src/containers/BoatContainer.jsx
@@ -0,0 +1,16 @@
+import { useState, useEffect } from 'react';
+import { Switch, Route, useHistory } from 'react-router-dom';
+
+import { getAllBoats, postBoat, putBoat, deleteBoat } from '../services/boats';
+import Boats from '../screens/Boats';
+import BoatCreate from '../screens/BoatCreate';
+import BoatEdit from '../screens/BoatEdit';
+import BoatDetail from '../screens/BoatDetail';
+
+export default function BoatContainer() {
+	const [boatList, setBoatList] = useState([]);
+	const history = useHistory();
+
+
+  
+}
