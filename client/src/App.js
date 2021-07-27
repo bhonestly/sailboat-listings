521c194e04eb583f3d780b3ea31d95ff0dd7f051
bhonestly <ernestbizzell@gmail.com>
2021-07-27 14:22:54 -0700

    frontend auth

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,53 @@
 import './App.css';
 import BoatContainer from './containers/BoatContainer'
+import { useState, useEffect } from 'react'
+import { useHistory, Switch, Route } from 'react-router-dom';
+import { loginUser, registerUser, verifyUser, removeToken } from './services/auth';
+import Login from './screens/Login/Login'
+import SignUp from './screens/SignUp/SignUp'
 
 function App() {
+  const [currentUser, setCurrentUser] = useState(null);
+  const history = useHistory();
+
+  useEffect(() => {
+    const handleVerify = async () => {
+      const currentUser = await verifyUser();
+      setCurrentUser(currentUser);
+    }
+    handleVerify();
+  }, [])
+
+  const handleLogin = async (formData) => {
+    const currentUser = await loginUser(formData);
+    setCurrentUser(currentUser);
+    history.push('/')
+  }
+
+  const handleRegister = async (formData) => {
+    const currentUser = await registerUser(formData);
+    setCurrentUser(currentUser);
+    history.push('/')
+  }
+
+  const handleLogout = () => {
+    localStorage.removeItem("authToken");
+    removeToken();
+    setCurrentUser(null);
+  }
   return (
     <div className="App">
-      <BoatContainer/>
+      <Switch>
+        <Route path='/login'>
+          <Login handleLogin={handleLogin} />
+        </Route>
+        <Route path='/signup'>
+          <SignUp handleRegister={handleRegister} />
+        </Route>
+        <Route path='/'>
+          <BoatContainer currentUser={currentUser} />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- client/src/screens/Login/Login.jsx
+++ client/src/screens/Login/Login.jsx
@@ -1,9 +1,52 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { Link } from 'react-router-dom';
 
-export default function Login() {
+export default function Login(props) {
+  const [formData, setFormData] = useState({
+    username: "",
+    password: ""
+  })
+  const { username, password } = formData;
+  const { handleLogin } = props;
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+    setFormData(prevState => ({
+      ...prevState,
+      [name]: value
+    }))
+  }
+
   return (
-    <div>
-      
+    <div className="auth-container" >
+      <h2>login</h2>
+      <hr />
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        handleLogin(formData);
+      }} >
+        <label>
+          Username:
+            <input
+            name="username"
+            type="text"
+            value={username}
+            onChange={handleChange}
+          />
+        </label>
+        <label>
+          Password:
+            <input
+            name="password"
+            type="password"
+            value={password}
+            onChange={handleChange}
+          />
+        </label>
+        <hr />
+        <button>Login</button>
+        <Link to="/register">Register</Link>
+      </form>
     </div>
   )
 }
